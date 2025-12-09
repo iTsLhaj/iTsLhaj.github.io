b3b516f4bc5c4f6c80e8501c20d1dba02a9f1ae5
Ryūken <115029856+iTsLhaj@users.noreply.github.com>
2025-12-08 19:57:48 -0500

    fix: build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { href, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { StaggerContainer, StaggerItem } from '../components/StaggerComponent';
 import { useEffect } from 'react';
 
